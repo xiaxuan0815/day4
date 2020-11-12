43fca342d76c4fcb4f672149fb257dbde6b0f442
3158438026@qq.com <3158438026@qq.com>
2020-11-12 15:24:14 +0800

    day4日考

--- bath-service/src/main/java/com/lihaiyang/ServiceStarter.java
+++ bath-service/src/main/java/com/lihaiyang/ServiceStarter.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ServiceStarter {
        public static void main(String[] args) {
-           SpringApplication.run(ServiceStarter.class,args);
+           SpringApplication.run(ServiceStarter.class, args);
        }
 }
